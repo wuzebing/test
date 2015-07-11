1a6a7fa63b97b42e61d0475bcbb7960807f52c36
Liu Qian Ming <2007ming.ming@163.com>
2015-07-12 00:11:53 +0800

    DEV:登录页面和首页

--- cview/src/main/webapp/resources/js/home/login/login.js
+++ cview/src/main/webapp/resources/js/home/login/login.js
@@ -1,109 +1,92 @@
-$(function() {
-	var user_name_remember = $.cookie("user_name_remember");
-	if(!(user_name_remember == null || user_name_remember == undefined || user_name_remember == "" || user_name_remember == "-1")){
-		$("#username").val(user_name_remember);
-		$("#user_name_remember").attr("checked",'true');
-	}else{
-		$("#user_name_remember").removeAttr("checked");
-	}
-	document.onkeydown=function(event){ 
-        e = event ? event :(window.event ? window.event : null); 
-        if(e.keyCode==13){ 
-            //执行的方法  
-        	$("#verifyText").blur();
-        	$("#loginIn").click();
-        } 
-    }; 
-	// 校验图片点击事件
-	$("#verifyImg").click(function() {
-		refreshVerify();
-	});
-	$("#verifyText").blur(function() {
-		verifyTextVerify(this);
-	});
-});
-
-// 刷新验证码
-function refreshVerify() {
-	alert(321);
-	debugger;
-	var url = cview.path
-			+ "/verifycode/getcode?width=87&height=34&codeCount=4&sessionName=loginVerify&it="
-			+ Math.random();
-	$("#verifyImg").attr("src", url);
-}
-function verifyTextVerify(obj){
-	var verifyCode = $(obj).val();
-	$.ajax({
-		url : cview.path + "/loginPage/validateVerify",
-		type : "post",
-		cache : false,
-		async : false,
-		dataType : "json",
-		data : {
-			verifyCode : verifyCode
-		},
-		traditional : true,
-		success : function(data, textStatus) {
-			if (data == true || data == "true") {
-				$("#error_Message").css("display", "none"); 
-			} else {
-				$("#error_Message").html("验证码不正确").css("display", "inline"); 
-			}
-		},
-		error : function(XMLHttpRequest, textStatus, errorThrown) {
-			alert("请求失败");
+var login = {
+	
+	init:function(){
+		var _this = this;
+		
+		var user_name_remember = $.cookie("user_name_remember");
+		if(!(user_name_remember == null || user_name_remember == undefined || user_name_remember == "" || user_name_remember == "-1")){
+			$("#username").val(user_name_remember);
+			$("#user_name_remember").attr("checked",'true');
+		}else{
+			$("#user_name_remember").removeAttr("checked");
 		}
-	});
-}
-
-//登录
-function loginIn() {
-	$.ajax({
-		url : cview.path + "/loginPage/user/login",
-		type : "post",
-		cache : false,
-		async : false,
-		dataType : "json",
-		data : {
-			username : $("#username").val(),
-			password : $("#password").val(),
-			verifyCode : $("#verifyText").val()
-		},
-		traditional : true,// 使用传统方式序列化
-		success : function(data, textStatus) {
-			if (data.statusCode == 200) {
-				$.cookie("token", data.returnObj, {
-					expires : 1,
-					path : '/'
-				});
-				//互联网注册不允许登录
-				/*var token = getToken();
-				if(token != null && token.userAttribute == "15"){
+		
+		$("#verifyText").blur(function() {
+			_this._verifyTextVerify(this);
+		});
+	},
+	
+	refreshVerify:function(){
+		var url = cview.path
+				+ "/verifycode/getcode?width=100&height=38&codeCount=4&sessionName=loginVerify&it="
+				+ Math.random();
+		$("#verifyImg").attr("src", url);
+	},
+	
+	_verifyTextVerify: function(obj){
+		var verifyCode = $(obj).val();
+		$.ajax({
+			url : cview.path + "/homePage/validateVerify",
+			type : "post",
+			cache : false,
+			async : false,
+			dataType : "json",
+			data : {
+				verifyCode : verifyCode
+			},
+			traditional : true,
+			success : function(data, textStatus) {
+				if (data == true || data == "true") {
+					$("#error_Message").css("display", "none"); 
+				} else {
+					$("#error_Message").css("display", "block");
+					$("#error_content").html('验证码不正确'); 
+				}
+			},
+			error : function(XMLHttpRequest, textStatus, errorThrown) {
+				alert("请求失败");
+			}
+		});
+	},
+	
+	//登录
+	loginIn:function() {
+		$.ajax({
+			url : cview.path + "/loginPage/user/login",
+			type : "post",
+			cache : false,
+			async : false,
+			dataType : "json",
+			data : {
+				username : $("#username").val(),
+				password : $("#password").val(),
+				verifyCode : $("#verifyText").val()
+			},
+			traditional : true,// 使用传统方式序列化
+			success : function(data, textStatus) {
+				if (data.statusCode == 200) {
 					$.cookie("token", data.returnObj, {
-						expires : -1,
+						expires : 1,
 						path : '/'
 					});
-					alert("非激活码用户暂时不提供登录。");
-					return;
-				}*/
-				var checkedFlag = document.getElementById( "user_name_remember" ).checked;
-				if(checkedFlag){
-					$.cookie("user_name_remember", $("#username").val());
-				}else{
-					$.cookie("user_name_remember","");
+					var checkedFlag = document.getElementById( "user_name_remember" ).checked;
+					if(checkedFlag){
+						$.cookie("user_name_remember", $("#username").val());
+					}else{
+						$.cookie("user_name_remember","");
+					}
+					location.href = cview.path + "/loginPage/home/loginIn";
+				} else {
+					$("#error_Message").css("display", "block");
+					$("#error_content").html(data.returnObj||'用户名或密码不正确'); 
 				}
-				var locationUrl = "";
-				if(kurl){
-					locationUrl = "?kurl="+kurl;
-				}
-				window.location.href = cview.path + "/loginPage/home/loginIn"+locationUrl;
-			} else {
-				$("#error_Message").html(data.returnObj).css("display", "inline"); 
+			},
+			error : function(XMLHttpRequest, textStatus, errorThrown) {
+				alert(errorThrown);
 			}
-		},
-		error : function(XMLHttpRequest, textStatus, errorThrown) {
-			alert(errorThrown);
-		}
-	});
-}
+		});
+	}
+
+	
+	
+};   
